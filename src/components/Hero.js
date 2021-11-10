7d1827d62856ae3f6712829b2d6a37e3607145a0
Florian Meneses <florian.meneses@gmail.com>
2021-11-10 12:14:18 -0800

    Added sass animation partials

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import apink from '../img/apink.jpg';
 
 export default function Hero() {
 
   return (
     <div className="app-hero">
       <h1>Hero</h1>
+      <img src={ apink } alt="Apink" />
     </div>
   );
 };
