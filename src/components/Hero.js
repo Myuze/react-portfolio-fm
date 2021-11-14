0dfd3b4a3fef1d7fc94da4acf597cecfffb6856c
Myuze <Myuze@users.noreply.github.com>
2021-11-13 23:00:13 -0800

    Updated hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import twice from '../img/twice-alcoholfree.jpg';
+import twice from '../img/abstract14095.jpg';
 
 export default function Hero() {
 
   return (
     <div className="app-hero">
-      <h1 className="app-heading">Hero</h1>
       <img src={ twice } alt="TWICE" />
     </div>
   );
