a4b5d70c4a531a2a088180594c7a5107c850d0e0
Myuze <Myuze@users.noreply.github.com>
2021-11-15 23:55:14 -0800

    Added all apps to portifolio

--- src/components/AppCard.js
+++ src/components/AppCard.js
@@ -1,23 +1,57 @@
 import React from 'react';
 
 export default function AppCard() {
-  const app = {
-    "app_name": "Team Profile Generator",
-    "app_subtitle": "Javascript/Inquirer",
-    "app_img_URL": "https://github.com/Myuze/team-profile-gen/raw/main/team_profile.jpg",
-    "app_repo_URL": "https://github.com/Myuze/team-profile-gen"
-  }
+  const apps = [
+    {
+      "app_name": "Tech Blog",
+      "app_subtitle": "JS/Bootstrap/SASS/Handlebars",
+      "app_img_URL": "https://github.com/Myuze/tech-blog-fm/raw/main/tech-blog.jpg",
+      "app_link_URL": "https://tech-blog-fm.herokuapp.com/",
+      "app_repo_URL": "https://github.com/Myuze/tech-blog-fm",
+    },
+    {
+      "app_name": "Team Profile Generator",
+      "app_subtitle": "Javascript/Inquirer",
+      "app_img_URL": "https://github.com/Myuze/team-profile-gen/raw/main/team_profile.jpg",
+      "app_repo_URL": "https://github.com/Myuze/team-profile-gen",
+    },
+    {
+      "app_name": "Weather Dashboard",
+      "app_subtitle": "Bootstrap/Javascript",
+      "app_img_URL": "https://github.com/Myuze/weather-fm/raw/main/weather.jpg",
+      "app_repo_URL": "https://github.com/Myuze/weather-fm",
+    },
+    {
+      "app_name": "Recipe Champion",
+      "app_subtitle": "Javascript/Foundation CSS",
+      "app_img_URL": "https://github.com/Myuze/recipe-champion/raw/main/recipe-champion.jpg",
+      "app_link_URL": "https://myuze.github.io/recipe-champion/docs/index.html",
+      "app_repo_URL": "https://github.com/Myuze/recipe-champion",
+    },
+    {
+      "app_name": "Note Taker",
+      "app_subtitle": "JS/Express",
+      "app_img_URL": "https://github.com/Myuze/notetaker-fm/raw/main/notetaker.jpg",
+      "app_link_URL": "https://notetaker-fm.herokuapp.com/",
+      "app_repo_URL": "https://github.com/Myuze/notetaker-fm",
+    }
+  ]
+  
   return (
-    <div id="app-1" class="app-card">
-      <a href={ app.app_repo_URL }>
-        <div class="app-text">
-          <h5>{ app.app_name }</h5>
-          <p>
-              { app.app_subtitle }
-          </p>
+    <div className="app-container">
+      {apps.map((app, i) => (
+        <div key={i} className="app-card">
+          <a href={ app.app_repo_URL }>
+            <div class="app-text">
+              <h5>{ app.app_name }</h5>
+              <p>
+                  { app.app_subtitle }
+              </p>
+            </div>
+            <img src={ app.app_img_URL } alt={ app.app_name } />
+          </a>
         </div>
-        <img src={ app.app_img_URL } alt={ app.app_name } />
-      </a>
+      ))}
     </div>
   );
 };
